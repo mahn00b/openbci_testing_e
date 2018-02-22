0e9549ec2519d5fe3dd81bf243f1716effa75f9c
myousif1 <myousif1@gulls.salisbury.edu>
2018-02-22 12:52:36 -0500

    Blink-test survey works!

--- source/plugins/blink-test-plugin.js
+++ source/plugins/blink-test-plugin.js
@@ -7,11 +7,11 @@ jsPsych.plugins['blink-test'] = (function(){
         parameters: {
             samples: 5, //number of blinks to take
             interval: 5, //number of intervals
-            sampling_time:2, //time to
-            randomized: false,
-            toggle_light:0,
-            light_on: "#fc9c92",
-            light_off : "#8c554f"
+            sample_duration:1, //time to keep light on
+            randomized: false,//allow random intervals
+            toggle_light:0,//constant to toggle light
+            light_on: "#fc9c92",//bright light color
+            light_off : "#8c554f"//dark light color
         }
     };
 
@@ -27,36 +27,47 @@ jsPsych.plugins['blink-test'] = (function(){
         if(typeof  trial.onSample === "function")  plugin.info.parameters.onSample = trial.onSample;
 
 
+
         plugin.info.parameters.sample_duration = trial.sample_duration || 1;//time for blink light on
         plugin.info.parameters.num_trials = trial.num_trials || 5;//number of trials to take
         plugin.info.parameters.random_interval = trial.random_interval === true;//if set to true then time between trials will be random
         plugin.info.parameters.time_interval = trial.time_interval || 3;//time interval between trials if not random
 
 
+        //TODO: Create css classes to align elements to the center.
        var element =  'When the test begins, you will see a circle light up(as seen below)'
            + ' <br><div id="circle" class="circle"></div><br> '
-           +  'We will take ' + plugin.info.parameters.samples + ' samples'
+           +  'We will take ' + plugin.info.parameters.samples + ' samples<br>'
             + 'Press Next To Start'
-            + "<button id='blink-next' class='jspsych-btn'>Next &gt;</button></div>";
+            + '<button id="blink-next" class="jspsych-btn">Next &gt;</button></div>';
 
 
+       //element to display content
        display_element.append(element);
 
 
-        var light_timer = setInterval(toggleLight, 1000);//toggle light every second as an example
+        var light_timer = setInterval(toggleLight, 500);//toggle light every half second as an example
 
+        //event handler for the "next" button click
         $('#blink-next').click(function(e){
-            clearInterval(light_timer);
-            if(plugin.toggle_light) toggleLight();
-            display_element.innerHTML = "";
+
+
+            clearInterval(light_timer);//clear example timer
+            if(plugin.toggle_light) toggleLight();//if light is on turn it off
+            display_element.empty();//empty instruction contents
+
+            //building test element and append to display
             element = '<br><div id="circle" class="circle"></div><br> '
                         +'Trials Left: <p id="num-trials">'+ plugin.info.parameters.samples +'</p>';
             display_element.append(element);
 
+            //create a time interval
             var time = plugin.info.parameters.time_interval * 1000;
 
-            if(plugin.info.parameters.random_interval) time = randomTime(plugin.info.parameters.time_interval);
+            //if time is randomized we will be a random time between 1 and chosen time_interval
+            if(plugin.info.parameters.randomized) time = randomTime(plugin.info.parameters.time_interval);
 
+            //time next trial
             setTimeout(performTrial, time);
         });
 
@@ -66,41 +77,51 @@ jsPsych.plugins['blink-test'] = (function(){
     };
 
 
-
+    /*
+    * peformTrial - this function starts a trial by turning the light on, and executing the
+    *               onSample() option.
+    * @return VOID
+    * */
     function performTrial(){
 
-        toggleLight();
+
+        toggleLight();//turn light on
 
 
         if(typeof plugin.info.parameters.onSample === 'undefined'){
-
+            //this will execute if event handler was not assigned
             console.log("sample this");
 
-        }else plugin.info.parameters.onSample();
+        }else plugin.info.parameters.onSample();//run custom event handler
 
 
-
-        setTimeout(endTrial, plugin.info.parameters.sampling_time);
+        //use sample_duration to hold the light on for a certain amount of time
+        setTimeout(endTrial, plugin.info.parameters.sample_duration*1000);
 
     }
 
 
-
+    /*
+     * endTrial - this function is called after trial is performed for the sample duration.
+     *            It ends the current trial and deducts 1 from the total amount of trials
+     * @return VOID
+     * */
     function endTrial(){
 
-        toggleLight();
+        toggleLight();//turn light off
 
-        plugin.info.parameters.num_trials--;
+        plugin.info.parameters.num_trials--;//deduct number of trials left
 
-
+        //if there are no more trials left end JsPsych Experiment
         if(plugin.info.parameters.num_trials === 0) jsPsych.endExperiment("Trials over");
         else{
 
-
+            //create a new time from the time_interval
             var time = plugin.info.parameters.time_interval * 1000;
-
+            //if randomized get a random time
             if(plugin.info.parameters.random_interval) time = randomTime(plugin.info.parameters.time_interval);
 
+            //set timer for next trial
             setTimeout(performTrial, time);
 
 
@@ -111,21 +132,13 @@ jsPsych.plugins['blink-test'] = (function(){
     }
 
 
-
-
-
-    function nextTrial(){
-
-    }
-
-
     /*
      * toggleLight - Changes color for circle html element
      * @return void
      * */
     function toggleLight() {
         $('#circle').css("background", (plugin.info.parameters.toggle_light ?  plugin.info.parameters.light_off: plugin.info.parameters.light_on));
-        plugin.info.parameters.toggle_light = (plugin.info.parameters.toggle_light%2 - 1);
+        plugin.info.parameters.toggle_light = (plugin.info.parameters.toggle_light === 0 ? 1 : 0);
     }
 
     /*
@@ -135,7 +148,7 @@ jsPsych.plugins['blink-test'] = (function(){
     * */
     function randomTime(time=5){
         time = Math.floor(Math.random() * Math.floor(time));
-        return time;
+        return time*1000;
     }
 
     return plugin;
